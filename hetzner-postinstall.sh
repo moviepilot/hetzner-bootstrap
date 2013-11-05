280c1400cd3c5bc67fc7409dbfe22daa55f96cab
Caspar Clemens Mierau <ccm@screenage.de>
2013-11-05 17:03:35 +0100

    initial basenode 2.7 release

---
+++ hetzner-postinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+apt-get update
+apt-get -y upgrade
+apt-get -y install byobu wget ruby1.8 libxmlrpc-ruby libopenssl-ruby libshadow-ruby1.8 libaugeas-ruby1.8 virt-what
+wget http://apt.puppetlabs.com/pool/precise/main/f/facter/facter_1.7.3-1puppetlabs1_amd64.deb
+wget http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet-common_2.7.23-1puppetlabs1_all.deb
+wget http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet_2.7.23-1puppetlabs1_all.deb
+dpkg -i facter_1.7.3-1puppetlabs1_amd64.deb
+dpkg -i puppet-common_2.7.23-1puppetlabs1_all.deb
+dpkg -i puppet_2.7.23-1puppetlabs1_all.deb
+mkdir /root/.ssh
+chmod 700 /root/.ssh
+wget -O /root/.ssh/authorized_keys https://raw.github.com/ctavan/hetzner-bootstrap/basenode-2.7/root-ssh-authorizedkeys.pub
+# please add you custom postinstall commands here
+# you should keep in mind, that this script is run inside a chrooted
+# envorinment of the freshly installed server, so every change here
+# is a direct change in the new server
