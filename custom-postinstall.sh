6f950bc30a6b12a9cd35f3e516b5afc89f1d8e52
Caspar Clemens Mierau <ccm@screenage.de>
2013-11-05 17:27:25 +0100

    initial hadoop-2.7 bootstrapper branch

--- custom-postinstall.sh
+++ custom-postinstall.sh
@@ -2,3 +2,12 @@
 # you should keep in mind, that this script is run inside a chrooted
 # envorinment of the freshly installed server, so every change here
 # is a direct change in the new server
+parted -s /dev/sda mkpart primary ext4 108GB 3001GB
+parted -s /dev/sdb mkpart primary ext4 108GB 3001GB
+mkfs.ext4 /dev/sda4
+mkfs.ext4 /dev/sdb4
+tune2fs -o journal_data_writeback /dev/sda4
+tune2fs -o journal_data_writeback /dev/sdb4
+echo "/dev/sda4 /data/1 ext4 noatime,data=writeback,barrier=0,nobh,errors=remount-ro 0 0" >> /etc/fstab
+echo "/dev/sdb4 /data/2 ext4 noatime,data=writeback,barrier=0,nobh,errors=remount-ro 0 0" >> /etc/fstab
+mkdir -p /data/1/ /data/2/
--- hetzner-postinstall.sh
+++ hetzner-postinstall.sh
@@ -10,8 +10,17 @@ dpkg -i puppet-common_2.7.23-1puppetlabs1_all.deb
 dpkg -i puppet_2.7.23-1puppetlabs1_all.deb
 mkdir /root/.ssh
 chmod 700 /root/.ssh
-wget -O /root/.ssh/authorized_keys https://raw.github.com/ctavan/hetzner-bootstrap/basenode-2.7/root-ssh-authorizedkeys.pub
+wget -O /root/.ssh/authorized_keys https://raw.github.com/ctavan/hetzner-bootstrap/hadoop-2.7/root-ssh-authorizedkeys.pub
 # please add you custom postinstall commands here
 # you should keep in mind, that this script is run inside a chrooted
 # envorinment of the freshly installed server, so every change here
 # is a direct change in the new server
+parted -s /dev/sda mkpart primary ext4 108GB 3001GB
+parted -s /dev/sdb mkpart primary ext4 108GB 3001GB
+mkfs.ext4 /dev/sda4
+mkfs.ext4 /dev/sdb4
+tune2fs -o journal_data_writeback /dev/sda4
+tune2fs -o journal_data_writeback /dev/sdb4
+echo "/dev/sda4 /data/1 ext4 noatime,data=writeback,barrier=0,nobh,errors=remount-ro 0 0" >> /etc/fstab
+echo "/dev/sdb4 /data/2 ext4 noatime,data=writeback,barrier=0,nobh,errors=remount-ro 0 0" >> /etc/fstab
+mkdir -p /data/1/ /data/2/
