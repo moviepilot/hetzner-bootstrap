d8f74399e7473841b46d13320dd9364a8cd61927
Christoph Tavan <dev@tavan.de>
2014-03-27 17:37:17 +0100

    Use 100% for the data partitions.

--- hetzner-postinstall.sh
+++ hetzner-postinstall.sh
@@ -15,8 +15,8 @@ wget -O /root/.ssh/authorized_keys https://raw.github.com/ctavan/hetzner-bootstr
 # you should keep in mind, that this script is run inside a chrooted
 # envorinment of the freshly installed server, so every change here
 # is a direct change in the new server
-parted -s /dev/sda mkpart primary ext4 108GB 3001GB
-parted -s /dev/sdb mkpart primary ext4 108GB 3001GB
+parted -s /dev/sda mkpart primary ext4 108GB 100%
+parted -s /dev/sdb mkpart primary ext4 108GB 100%
 mkfs.ext4 /dev/sda4
 mkfs.ext4 /dev/sdb4
 tune2fs -o journal_data_writeback /dev/sda4
