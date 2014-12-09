a1a9eca5c0f2064d5058e2836319d8e3bb8a59de
Caspar Clemens Mierau <ccm@screenage.de>
2014-12-09 09:36:42 +0100

    bumping puppet to 2.7.26 and facter to 1.7.6

--- hetzner-postinstall.sh
+++ hetzner-postinstall.sh
@@ -2,12 +2,12 @@
 apt-get update
 apt-get -y upgrade
 apt-get -y install byobu wget ruby1.8 libxmlrpc-ruby libopenssl-ruby libshadow-ruby1.8 libaugeas-ruby1.8 virt-what
-wget http://apt.puppetlabs.com/pool/precise/main/f/facter/facter_1.7.3-1puppetlabs1_amd64.deb
-wget http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet-common_2.7.23-1puppetlabs1_all.deb
-wget http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet_2.7.23-1puppetlabs1_all.deb
-dpkg -i facter_1.7.3-1puppetlabs1_amd64.deb
-dpkg -i puppet-common_2.7.23-1puppetlabs1_all.deb
-dpkg -i puppet_2.7.23-1puppetlabs1_all.deb
+wget http://apt.puppetlabs.com/pool/precise/main/f/facter/facter_1.7.6-1puppetlabs1_amd64.deb
+wget http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet-common_2.7.26-1puppetlabs1_all.deb
+wget http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet_2.7.26-1puppetlabs1_all.deb
+dpkg -i facter_1.7.6-1puppetlabs1_amd64.deb
+dpkg -i puppet-common_2.7.26-1puppetlabs1_all.deb
+dpkg -i puppet_2.7.26-1puppetlabs1_all.deb
 mkdir /root/.ssh
 chmod 700 /root/.ssh
 wget -O /root/.ssh/authorized_keys https://raw.github.com/ctavan/hetzner-bootstrap/basenode/root-ssh-authorizedkeys.pub
