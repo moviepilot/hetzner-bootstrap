37b37617ff17893543b2cfe667f832e848adb27f
Christoph Tavan <dev@tavan.de>
2015-02-02 08:34:53 +0100

    Install the HWE kernel on basenode.

--- hetzner-postinstall.sh
+++ hetzner-postinstall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 apt-get update
 apt-get -y upgrade
+apt-get -y install --install-recommends linux-generic-lts-trusty
 apt-get -y install byobu wget ruby1.8 libxmlrpc-ruby libopenssl-ruby libshadow-ruby1.8 libaugeas-ruby1.8 virt-what
 wget http://apt.puppetlabs.com/pool/precise/main/f/facter/facter_1.7.6-1puppetlabs1_amd64.deb
 wget http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet-common_2.7.26-1puppetlabs1_all.deb
