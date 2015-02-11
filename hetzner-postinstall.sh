370d8eea915821f21398a647c823889c81641b52
Christoph Tavan <dev@tavan.de>
2015-02-11 18:48:52 +0100

    No need for hiera on puppet agents.

--- hetzner-postinstall.sh
+++ hetzner-postinstall.sh
@@ -7,14 +7,10 @@ apt-get -y install tmux curl wget ruby ruby1.8 libxmlrpc-ruby libopenssl-ruby li
 wget -O /tmp/facter_1.7.3-1puppetlabs1_amd64.deb http://apt.puppetlabs.com/pool/precise/main/f/facter/facter_1.7.3-1puppetlabs1_amd64.deb
 wget -O /tmp/puppet-common_2.7.26-1puppetlabs1_all.deb http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet-common_2.7.26-1puppetlabs1_all.deb
 wget -O /tmp/puppet_2.7.26-1puppetlabs1_all.deb http://apt.puppetlabs.com/pool/precise/main/p/puppet/puppet_2.7.26-1puppetlabs1_all.deb
-wget -O /tmp/hiera_1.3.4-1puppetlabs1_all.deb http://apt.puppetlabs.com/pool/precise/main/h/hiera/hiera_1.3.4-1puppetlabs1_all.deb
-wget -O /tmp/hiera-puppet_1.0.0-1puppetlabs1_all.deb http://apt.puppetlabs.com/pool/precise/main/h/hiera-puppet/hiera-puppet_1.0.0-1puppetlabs1_all.deb
 
 dpkg -i /tmp/facter_1.7.3-1puppetlabs1_amd64.deb
 dpkg -i /tmp/puppet-common_2.7.26-1puppetlabs1_all.deb
 dpkg -i /tmp/puppet_2.7.26-1puppetlabs1_all.deb
-dpkg -i /tmp/hiera_1.3.4-1puppetlabs1_all.deb
-dpkg -i /tmp/hiera-puppet_1.0.0-1puppetlabs1_all.deb
 
 rm -rf /tmp/*.deb
 
