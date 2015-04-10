e7945c0194777c316f426dd1060781369650b250
Caspar Clemens Mierau <ccm@screenage.de>
2015-04-10 12:51:54 +0200

    initial branch commit

---
+++ hetzner-postinstall.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# install bacic tools
+apt-get update
+apt-get -y dist-upgrade
+apt-get -y install byobu curl wget
+
+# install puppet
+wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+dpkg -i puppetlabs-release-trusty.deb
+apt-get update
+apt-get puppet
+sed -i '/templatedir/a pluginsync=true' /etc/puppet/puppet.conf
+
+# install default ssh keys
+mkdir /root/.ssh
+chmod 700 /root/.ssh
+wget -O /root/.ssh/authorized_keys https://raw.github.com/moviepilot/hetzner-bootstrap/puppet-express/root-ssh-authorizedkeys.pub
