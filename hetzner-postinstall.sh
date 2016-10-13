f0005535a39b5a60a75e85c41d2bb824f78aaff8
Caspar Clemens Mierau <ccm@screenage.de>
2016-10-13 11:22:16 +0200

    fixing ssh key url

--- hetzner-postinstall.sh
+++ hetzner-postinstall.sh
@@ -15,7 +15,7 @@ apt-get -y install puppet-agent=1.4.2-1xenial tmux
 # install default ssh keys
 mkdir /root/.ssh
 chmod 700 /root/.ssh
-wget -O /root/.ssh/authorized_keys https://github.com/moviepilot/hetzner-bootstrap/blob/puppet-express-16.04/root-ssh-authorizedkeys.pub
+wget -O /root/.ssh/authorized_keys https://raw.githubusercontent.com/moviepilot/hetzner-bootstrap/puppet-express-16.04/root-ssh-authorizedkeys.pub
 
 echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/opt/puppetlabs/bin"' > /etc/environment
 update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
